a229338cb467bd0d1f3260a2929174a4b6991889
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2025-02-03 21:40:13 +0300

    Initial Commit

---
+++ LetSki/LetSkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetSkiApp.swift
+//  LetSki
+//
+//  Created by Eugene Kolesnikov on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LetSkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
